93060b7f0d6d7cf3548cc5befced73d053ed6ae5
ZAIM@WSL2 <zaim.azhar97@gmail.com>
2022-10-24 22:04:15 +0800

    Add drawing folder

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -12,10 +12,17 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+        <!-- Styles -->
+        @livewireStyles
     </head>
     <body>
         <div class="font-sans text-gray-900 antialiased">
             {{ $slot }}
         </div>
+
+        @stack('guest')
+
+        @livewireScripts
     </body>
 </html>
